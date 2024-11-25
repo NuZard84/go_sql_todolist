f9b478aa2a930b5381b42afa5cf69959dd270b9e
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2024-11-25 12:53:03 +0530

    create items add api made

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/NuZard84/go_sql_todolist
+
+go 1.22.4
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+)
---
+++ todolist.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	log "github.com/sirupsen/logrus"
+)
+
+var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
+	Description string
+	Completed   bool
+}
+
+func getLists(w http.ResponseWriter, r *http.Request) {
+
+	log.Info("API getlist OK")
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database.")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
+}
+
+// It runs automatically once per package, even before the main() function is executed.
+// It is used to set up the logger or db connection and more.
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetReportCaller(true)
+}
+
+func main() {
+	defer db.Close()
+
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
+	fmt.Println("Starting the application...")
+	log.Info("Starting the application server...")
+	router := mux.NewRouter()
+	router.HandleFunc("/getlist", getLists).Methods("GET")
+	router.HandleFunc("/additem", CreateItem).Methods("POST")
+	http.ListenAndServe(":8000", router)
+}
