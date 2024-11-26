d6dc7b8d205ce4b94b6dc1326c6c2b10da350d52
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2024-11-26 19:32:26 +0530

    complete the backend

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- todolist.go
+++ todolist.go
@@ -5,15 +5,17 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/rs/cors"
 	log "github.com/sirupsen/logrus"
 )
 
-var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+var db *gorm.DB
 
 type TodoItemModel struct {
 	Id          int `gorm:"primary_key"`
@@ -38,6 +40,13 @@ func getItemById(Id int) bool {
 	return true
 }
 
+func getTodoItems(completed bool) interface{} {
+
+	var todos []TodoItemModel
+	TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
+	return TodoItems
+}
+
 func CreateItem(w http.ResponseWriter, r *http.Request) {
 	description := r.FormValue("description")
 	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database.")
@@ -48,11 +57,74 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result.Value)
 }
 
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	Id, _ := strconv.Atoi(vars["Id"])
+	isItemExist := getItemById(Id)
+
+	if !isItemExist {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"message": "Item not found"}`)
+		return
+	} else {
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"Id": Id, "completed": completed}).Info("Update TodoItem. Saving to database.")
+		todo := &TodoItemModel{}
+		db.First(&todo, Id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"message": "Item updated successfully"}`)
+	}
+
+}
+
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["Id"])
+
+	isItemExist := getItemById(id)
+	if !isItemExist {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"message": "Item not found"}`)
+		return
+	} else {
+		log.WithFields(log.Fields{"Id": id}).Info("Delete TodoItem. Saving to database.")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		db.Delete(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"message": "Item deleted successfully"}`)
+	}
+}
+
+func GetCompletedITems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get completed TodoItems")
+	completedTodoItems := getTodoItems(true)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(completedTodoItems)
+}
+
+func GetIncompletedItems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get incompleted TodoItems")
+	incompletedTodoItems := getTodoItems(false)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(incompletedTodoItems)
+}
+
 // It runs automatically once per package, even before the main() function is executed.
 // It is used to set up the logger or db connection and more.
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
+
+	var err error
+	db, err = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database %v", err)
+	}
+	log.Info("Database connection successful")
 }
 
 func main() {
@@ -64,7 +136,16 @@ func main() {
 	fmt.Println("Starting the application...")
 	log.Info("Starting the application server...")
 	router := mux.NewRouter()
-	router.HandleFunc("/getlist", getLists).Methods("GET")
-	router.HandleFunc("/additem", CreateItem).Methods("POST")
-	http.ListenAndServe(":8000", router)
+	router.HandleFunc("/healthz", getLists).Methods("GET")
+	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo-completed", GetCompletedITems).Methods("GET")
+	router.HandleFunc("/todo-incomplete", GetIncompletedItems).Methods("GET")
+	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
+
+	handler := cors.New(cors.Options{
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
+	}).Handler(router)
+	http.ListenAndServe(":8000", handler)
 }
